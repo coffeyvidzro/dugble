cfdcc484a565d8bfdce6983acfe4a8832dcc3eaa
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-07-22 16:54:27 +0000

    Add SMS provider integration and routing

--- server/internal/sms/provider/arkesel/provider.go
+++
@@ -1,5 +0,0 @@
-package arkesel
-
-import "github.com/coffeyvidzro/dugble/server/internal/sms/provider"
-
-var _ provider.Provider = (*Client)(nil)
--- server/internal/sms/provider/mnotify/types.go
+++
@@ -1,9 +0,0 @@
-package mnotify
-
-type sendResponse struct {
-	Code      string `json:"code"`
-	Status    string `json:"status"`
-	Message   string `json:"message"`
-	MessageID string `json:"message_id"`
-	ID        string `json:"id"`
-}
