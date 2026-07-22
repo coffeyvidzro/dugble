b8f0b15652e389ced794fdb9ef0b2a9e210d5cc0
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-07-22 19:11:12 +0000

    Use dedicated wallet routes

--- server/internal/modules/wallet/routes.go
+++ server/internal/modules/wallet/routes.go
@@ -9,7 +9,7 @@ import (
 type TenantMiddleware func(permission tenant.Permission) echo.MiddlewareFunc
 
 func RegisterRoutes(router *echo.Echo, handler *Handler, authMiddleware echo.MiddlewareFunc, csrfMiddleware echo.MiddlewareFunc, tenantMiddleware TenantMiddleware) {
-	wallet := router.Group("/teams/:team_id/wallet")
+	wallet := router.Group("/wallet")
 	wallet.Use(authMiddleware, csrfMiddleware)
 	wallet.GET("", handler.Get, tenantMiddleware(tenant.PermissionWalletRead))
 	wallet.GET("/transactions", handler.ListTransactions, tenantMiddleware(tenant.PermissionWalletRead))
