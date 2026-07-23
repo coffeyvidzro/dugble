4c013e656889b39b5453c62e77e5c94e94e6bc93
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-07-23 15:14:45 +0000

    Keep SMS refund failures retryable

--- server/internal/delivery/sms/worker.go
+++ server/internal/delivery/sms/worker.go
@@ -33,16 +33,14 @@ func (w *Worker) Work(ctx context.Context, job *river.Job[DeliverArgs]) error {
 
 	response, err := w.sender.Send(ctx, smsapi.SendRequest{To: message.To, From: message.From, Message: message.Body})
 	if err != nil {
-		failed, updateErr := w.repository.MarkFailed(ctx, job.Args.MessageID, job.Args.TeamID, err.Error())
-		if updateErr != nil {
-			return updateErr
-		}
-		_, refundErr := w.wallet.RefundSMSCharge(ctx, job.Args.TeamID, message.CostMicros, job.Args.MessageID, message.Metadata)
-		if refundErr != nil {
+		// Keep the message retryable until the compensating refund succeeds.
+		// If we mark it failed first and the refund fails, the next River retry will
+		// skip the job because failed messages are no longer claimable for delivery.
+		if _, refundErr := w.wallet.RefundSMSCharge(ctx, job.Args.TeamID, message.CostMicros, job.Args.MessageID, message.Metadata); refundErr != nil {
 			return errors.Join(err, refundErr)
 		}
-		_ = failed
-		return nil
+		_, updateErr := w.repository.MarkFailed(ctx, job.Args.MessageID, job.Args.TeamID, err.Error())
+		return updateErr
 	}
 
 	_, err = w.repository.MarkSubmitted(ctx, job.Args.MessageID, job.Args.TeamID, response.ProviderID, response.ProviderMsgID, smsmodule.MapProviderStatus(response.Status))
