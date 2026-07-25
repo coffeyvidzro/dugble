faa1637d8528ce3971fdfca251a8e192e810ddfb
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-07-25 00:17:56 +0000

    Model SMS pricing by country

--- server/internal/backoffice/smspricing/model.go
+++ server/internal/backoffice/smspricing/model.go
@@ -13,10 +13,7 @@ type PlanRow struct {
 	Currency            string
 	IsDefault           bool
 	Status              string
-	LocalRateMicros     int64
-	HasCurrentLocalRate bool
-	A2PRateMicros       int64
-	HasCurrentA2PRate   bool
+	CurrentCountryCount int64
 	CreatedAt           time.Time
 	UpdatedAt           time.Time
 }
@@ -28,16 +25,21 @@ type PlanOption struct {
 }
 
 type RateRow struct {
-	ID             string
-	TrafficClass   string
-	UnitCostMicros int64
-	EffectiveFrom  time.Time
-	EffectiveUntil *time.Time
-	Status         string
-	Lifecycle      string
-	CanEdit        bool
-	CanCancel      bool
-	CreatedAt      time.Time
+	ID                 string
+	DestinationCountry string
+	UnitCostMicros     int64
+	EffectiveFrom      time.Time
+	EffectiveUntil     *time.Time
+	Status             string
+	Lifecycle          string
+	CanEdit            bool
+	CanCancel          bool
+	CreatedAt          time.Time
+}
+
+type CountryRateTimeline struct {
+	CountryCode string
+	Rates       []RateRow
 }
 
 type AuditRow struct {
@@ -52,8 +54,7 @@ type AuditRow struct {
 type PlanDetail struct {
 	Plan              PlanRow
 	Rates             []RateRow
-	LocalRates        []RateRow
-	A2PRates          []RateRow
+	CountryRates      []CountryRateTimeline
 	Audits            []AuditRow
 	AssignedTeamCount int64
 	RateCount         int64
@@ -70,10 +71,10 @@ type RenamePlanRequest struct {
 }
 
 type AddRateRequest struct {
-	TrafficClass   string
-	UnitCostUSD    string
-	EffectiveFrom  string
-	EffectiveUntil string
+	DestinationCountry string
+	UnitCostUSD         string
+	EffectiveFrom      string
+	EffectiveUntil     string
 }
 
 type UpdateRateRequest struct {
@@ -83,31 +84,25 @@ type UpdateRateRequest struct {
 }
 
 type RatePreview struct {
-	PlanID            string
-	PlanName          string
-	TrafficClass      string
-	UnitCostUSD       string
-	UnitCostMicros    int64
-	EffectiveFrom     time.Time
-	EffectiveUntil    *time.Time
-	CurrentRateMicros int64
-	HasCurrentRate    bool
+	PlanID              string
+	PlanName            string
+	DestinationCountry  string
+	UnitCostUSD         string
+	UnitCostMicros      int64
+	EffectiveFrom       time.Time
+	EffectiveUntil      *time.Time
+	CurrentRateMicros   int64
+	HasCurrentRate      bool
 }
 
 type TeamConfiguration struct {
-	TeamID              string
-	PricingPlanID       string
-	PricingPlanName     string
-	DefaultTrafficClass string
-	LocalEnabled        bool
-	A2PEnabled          bool
-	Explicit            bool
-	UpdatedAt           *time.Time
+	TeamID          string
+	PricingPlanID   string
+	PricingPlanName string
+	Explicit        bool
+	UpdatedAt       *time.Time
 }
 
 type UpdateTeamRequest struct {
-	PricingPlanID       string
-	DefaultTrafficClass string
-	LocalEnabled        bool
-	A2PEnabled          bool
+	PricingPlanID string
 }
