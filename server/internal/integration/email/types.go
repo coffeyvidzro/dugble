4777f1836f30fa6e98e8f92e22f5983a4f7f9560
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-07-28 09:45:04 +0000

    refactor: define SES integration types

---
+++ server/internal/integration/email/types.go
@@ -0,0 +1,36 @@
+package email
+
+import (
+	"context"
+	"errors"
+	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/service/ses"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+)
+
+const ProviderSES = "ses"
+
+var ErrUnsupportedAttachmentPath = errors.New("attachment paths are not supported by the SES integration")
+
+type sesAPI interface {
+	SendRawEmail(context.Context, *ses.SendRawEmailInput, ...func(*ses.Options)) (*ses.SendRawEmailOutput, error)
+}
+
+type sesIdentityAPI interface {
+	CreateEmailIdentity(context.Context, *sesv2.CreateEmailIdentityInput, ...func(*sesv2.Options)) (*sesv2.CreateEmailIdentityOutput, error)
+	PutEmailIdentityMailFromAttributes(context.Context, *sesv2.PutEmailIdentityMailFromAttributesInput, ...func(*sesv2.Options)) (*sesv2.PutEmailIdentityMailFromAttributesOutput, error)
+	GetEmailIdentity(context.Context, *sesv2.GetEmailIdentityInput, ...func(*sesv2.Options)) (*sesv2.GetEmailIdentityOutput, error)
+}
+
+// Client implements provider-neutral sending and sender-domain operations using AWS SES.
+type Client struct {
+	defaultRegion string
+	defaultFrom   string
+	accessKey     string
+	secretKey     string
+
+	mu              sync.Mutex
+	sendingClients  map[string]sesAPI
+	identityClients map[string]sesIdentityAPI
+}
