f95867391e95409281c04c98878e59cb71601879
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-07-31 19:23:00 +0000

    harden SES raw email delivery

--- server/internal/integration/aws/ses/errors.go
+++ server/internal/integration/aws/ses/errors.go
@@ -2,4 +2,8 @@ package ses
 
 import "errors"
 
-var ErrUnsupportedAttachmentPath = errors.New("attachment paths are not supported by the SES integration")
+var (
+	ErrMessageTooLarge          = errors.New("email exceeds the SES raw message size limit")
+	ErrReservedHeader           = errors.New("email header is reserved by the SES integration")
+	ErrUnsupportedAttachmentPath = errors.New("attachment paths are not supported by the SES integration")
+)
