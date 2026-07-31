ddf0311ed39e82e48375b573f333bec79c468eb8
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-07-31 19:26:05 +0000

    format SES hardening changes

--- server/internal/integration/aws/ses/errors.go
+++ server/internal/integration/aws/ses/errors.go
@@ -3,7 +3,7 @@ package ses
 import "errors"
 
 var (
-	ErrMessageTooLarge          = errors.New("email exceeds the SES raw message size limit")
-	ErrReservedHeader           = errors.New("email header is reserved by the SES integration")
+	ErrMessageTooLarge           = errors.New("email exceeds the SES raw message size limit")
+	ErrReservedHeader            = errors.New("email header is reserved by the SES integration")
 	ErrUnsupportedAttachmentPath = errors.New("attachment paths are not supported by the SES integration")
 )
