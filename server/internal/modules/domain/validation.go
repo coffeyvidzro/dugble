06888e343f45ea38fcc8b97324f1244114f64f41
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-01 11:46:14 +0000

    feat(domain): update supported SES regions

--- server/internal/modules/domain/validation.go
+++ server/internal/modules/domain/validation.go
@@ -12,10 +12,14 @@ import (
 const maxDomainLength = 253
 
 var (
-	domainPattern    = regexp.MustCompile(`^[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)+$`)
-	labelPattern     = regexp.MustCompile(`^[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?$`)
+	domainPattern = regexp.MustCompile(`^[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)+$`)
+	labelPattern  = regexp.MustCompile(`^[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?$`)
 	supportedRegions = map[string]struct{}{
-		"eu-north-1": {}, "us-east-1": {}, "eu-west-1": {}, "sa-east-1": {}, "ap-northeast-1": {},
+		"us-east-1":      {},
+		"ap-northeast-1": {},
+		"eu-north-1":     {},
+		"sa-east-1":      {},
+		"af-south-1":     {},
 	}
 )
 
