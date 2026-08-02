0017890d5c96d611d427a09e3919ef483c9bb1bf
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-02 23:48:30 +0000

    chore(migrations): remove squashed webhook lifecycle migration

--- server/migrations/025_harden_webhook_delivery_lifecycle.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE webhook_deliveries
-    DROP CONSTRAINT IF EXISTS chk_webhook_delivery_status;
-
-ALTER TABLE webhook_deliveries
-    ADD CONSTRAINT chk_webhook_delivery_status CHECK (
-        status IN ('pending', 'retrying', 'succeeded', 'failed', 'canceled')
-    );
