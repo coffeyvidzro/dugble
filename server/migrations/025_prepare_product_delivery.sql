154a9cc8b0baf2fb12ac22d377f6b838fc95d513
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-04 06:18:33 +0000

    refactor(migrations): remove product delivery preparation migration

--- server/migrations/025_prepare_product_delivery.sql
+++
@@ -1,5 +0,0 @@
-CREATE UNIQUE INDEX IF NOT EXISTS idx_email_messages_id_team
-    ON email_messages (id, team_id);
-
-CREATE UNIQUE INDEX IF NOT EXISTS idx_sms_messages_id_team
-    ON sms_messages (id, team_id);
